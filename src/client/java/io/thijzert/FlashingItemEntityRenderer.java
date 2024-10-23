09d2be852e0614a9e139c6d831f6c8f977a5eff2
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2024-10-23 18:50:16 +0200

    Update to Minecraft 1.21.2

--- src/client/java/io/thijzert/FlashingItemEntityRenderer.java
+++ src/client/java/io/thijzert/FlashingItemEntityRenderer.java
@@ -4,24 +4,31 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.ItemEntityRenderer;
+import net.minecraft.client.render.entity.state.ItemEntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.ItemEntity;
 
 public class FlashingItemEntityRenderer extends ItemEntityRenderer {
+    private ItemEntity entity_;
+
     public FlashingItemEntityRenderer(final EntityRendererFactory.Context context) {
         super(context);
     }
 
     @Override
-    public void render(final ItemEntity entity,
-                       final float entityYaw,
-                       final float partialTicks,
+    public void updateRenderState(final ItemEntity itemEntity, final ItemEntityRenderState itemEntityRenderState, float f) {
+        entity_ = itemEntity;
+        super.updateRenderState(itemEntity, itemEntityRenderState, f);
+    }
+
+    @Override
+    public void render(final ItemEntityRenderState state,
                        final MatrixStack matrixStack,
                        final VertexConsumerProvider vertexConsumerProvider,
                        final int packedLight) {
         if (ClearDespawnReworkedClient.config.modEnabled) {
             if (ClearDespawnReworkedClient.config.flashingEnabled) {
-                final int remainingTime = ClearDespawnReworkedClient.config.despawnAge - entity.getItemAge();
+                final int remainingTime = ClearDespawnReworkedClient.config.despawnAge - entity_.getItemAge();
                 if (remainingTime < ClearDespawnReworkedClient.config.getFlashStartTime() && remainingTime > 0) {
                     int flashSpeed = ClearDespawnReworkedClient.config.getFlashSpeed();
                     if (ClearDespawnReworkedClient.config.urgentFlashing) {
@@ -38,12 +45,12 @@ public class FlashingItemEntityRenderer extends ItemEntityRenderer {
         } else {
             this.shadowOpacity = 0.75F;
         }
-        super.render(entity, entityYaw, partialTicks, matrixStack, vertexConsumerProvider, packedLight);
+        super.render(state, matrixStack, vertexConsumerProvider, packedLight);
     }
 
     public static class Factory implements EntityRendererFactory<ItemEntity> {
         @Override
-        public EntityRenderer<ItemEntity> create(final Context context) {
+        public EntityRenderer<ItemEntity, ?> create(final Context context) {
             return new FlashingItemEntityRenderer(context);
         }
     }
